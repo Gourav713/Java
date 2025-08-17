fe38f55a475465ee0ab26303f124c5ad771e6402
Gourav verma <160299664+Gourav713@users.noreply.github.com>
2025-08-17 10:23:02 +0530

    done

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,13 +1,14 @@
-public class Factorial{
-    public static int fact(int num){
-        if(num ==0){
+public class Factorial {
+    public static int fact(int num) {
+        if (num == 0) {
             return 1;
         }
-        int fnm1 = fact(num-1);
+        int fnm1 = fact(num - 1);
         int fn = num * fnm1;
         return fn;
     }
+
     public static void main(String[] args) {
-    System.err.println(fact(10));
-}
+        System.err.println(fact(10));
+    }
 }
